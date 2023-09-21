1aeaab32720144f6921cc8a2e5a6d0bd76143c2b
IvyJepkirui <101108464+Ivy-Jepkirui@users.noreply.github.com>
2023-09-21 12:04:22 +0300

    Update Task10.c

--- Task10.c
+++ Task10.c
@@ -3,68 +3,18 @@
 #include <unistd.h>
 #include <string.h>
 #include <limits.h>
-#include <errno.h>
-
-// Define a custom structure for shell-related information
-typedef struct {
-    char *argv[100]; // Example field, adjust as needed
-    int status;      // Example field, shell status
-    int err_num;     // Example field, error number
-} info_t;
-
-// Function prototypes
-int my_exit(info_t *info);
-int my_cd(info_t *info);
-int my_help(info_t *info);
-
-int main(int argc, char *argv[]) {
-    // Initialize the info_t structure with appropriate values
-    info_t shell_info;
-    memset(&shell_info, 0, sizeof(info_t)); // Initialize all fields to zero or NULL
-
-    // Set argv in the info_t structure with the command-line arguments
-    for (int i = 0; i < argc && i < 100; i++) {
-        shell_info.argv[i] = argv[i];
-    }
-
-    // Example usage of the shell functions
-    int exit_code = my_exit(&shell_info);
-    int cd_result = my_cd(&shell_info);
-    int help_result = my_help(&shell_info);
-
-    // Handle the results as needed
-
-    return 0;
-}
-
-int my_exit(info_t *info) {
-    int exit_status;
-
-    if (info->argv[1]) {
-        // Implement _strtoi to convert a string to an integer
-        exit_status = _strtoi(info->argv[1]);
-        if (exit_status == -1) {
-            info->status = 2;
-            fprintf(stderr, "Illegal number: %s\n", info->argv[1]);
-            return 1;
-        }
-        info->err_num = exit_status;
-        return -2;
-    }
-
-    info->err_num = -1;
-    return -2;
-}
 
 int my_cd(info_t *info) {
-    char *current_dir, *new_dir, buffer[1024];
+    char *current_dir, *new_dir, buffer[PATH_MAX];
     int chdir_result;
 
-    current_dir = getcwd(buffer, 1024);
-    if (!current_dir)
+    current_dir = getcwd(buffer, PATH_MAX);
+    if (!current_dir) {
         fprintf(stderr, "TODO: Handle getcwd failure here\n");
+        return 1;
+    }
 
-    if (!info->argv[1]) {
+    if (!info->argv[1] || strcmp(info->argv[1], "~") == 0) {
         new_dir = _getenv(info, "HOME=");
         if (!new_dir)
             chdir_result = chdir((new_dir = _getenv(info, "PWD=")) ? new_dir : "/");
@@ -82,18 +32,12 @@ int my_cd(info_t *info) {
     }
 
     if (chdir_result == -1) {
-        fprintf(stderr, "can't cd to %s\n", info->argv[1]);
-    } else {
-        _setenv(info, "OLDPWD", _getenv(info, "PWD="));
-        _setenv(info, "PWD", getcwd(buffer, 1024));
+        perror("cd");
+        return 1;
     }
 
-    return 0;
-}
+    _setenv(info, "OLDPWD", current_dir);
+    _setenv(info, "PWD", getcwd(buffer, PATH_MAX));
 
-int my_help(info_t *info) {
-    (void)info; // Avoid unused parameter warning
-
-    printf("Help is not yet implemented\n");
     return 0;
 }
